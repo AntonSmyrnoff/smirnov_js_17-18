708bd272f0c23d89b7cc1be322e718c59d59378e
AntonSmyrnoff <a.smyrnoff@gmail.com>
2017-02-01 23:28:06 +0200

    ready for gulp

---
+++ GRUNT/gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/src/*.js'],
+        dest: 'js/dist/script.min.js'
+      }
+    },
+    uglify: {
+      dist: {
+        src: ['js/dist/script.min.js'],
+        dest: 'js/dist/script.min.js'
+      }
+    },
+    concat_css: {
+      options: {},
+      all: {
+        src: ["css/src/*.css"],
+        dest: "css/dist/style.min.css"
+      },
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'css/dist/style.min.css': ['css/dist/style.min.css']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+  grunt.registerTask('default', ['concat', 'uglify', 'concat_css', 'cssmin']);
+
+};
