dfe1971e0e2ae67471ffea2704bc1d8ef19740b8
AntonSmyrnoff <a.smyrnoff@gmail.com>
2017-02-01 22:17:22 +0200

    with .gitignore

--- gruntfile.js
+++ gruntfile.js
@@ -15,12 +15,33 @@ module.exports = function(grunt) {
         src: ['js/dist/script.min.js'],
         dest: 'js/dist/script.min.js'
       }
+    },
+    concat_css: {
+      options: {},
+      all: {
+        src: ["css/src/*.css"],
+        dest: "css/dist/style.min.css"
+      },
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'css/dist/style.min.css': ['css/dist/style.min.css']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+
+  grunt.registerTask('default', ['concat', 'uglify', 'concat_css', 'cssmin']);
 
 };
